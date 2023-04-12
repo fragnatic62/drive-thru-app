7142a9fd3b921d7d34463d45e0c9324f4fb263ef
Douie Villegas <fragnatic62@gmail.com>
2023-04-13 06:55:31 +0800

    add events and function place holder

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 from kivy.core.window import Window
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.properties import ObjectProperty
+from kivy.properties import ObjectProperty, StringProperty
 from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.image import Image
@@ -13,20 +13,32 @@ Window.maximize()
 
 Builder.load_file(os.path.abspath('main.kv'))
 
-class OrderTakerCamera(BoxLayout):
-    pass
-
-class CustomerCamera(BoxLayout):
-    pass
-
-
-class AdvertisementSpace(BoxLayout):
-    pass
-
 class FullImage(Image):
     pass
+    
 class AppLayout(Widget):
-    pass
+    img_src = StringProperty('assets/test-image.png')
+    video_src = StringProperty('assets/big_buck_bunny_720p_1mb.mp4')
+
+    # handles the streaming/image event on advertisement component section
+    def ads_component_handler(self):
+        if(self.img_src=='assets/test.png'):
+            self.img_src = 'assets/test-image.png'
+        else:
+            self.img_src = 'assets/test.png'
+
+    # handles the streaming event on customer and ordertaker component section
+    def video_component_handler(self):
+        if(self.video_src=='assets/big_buck_bunny_720p_1mb.mp4'):
+            self.video_src = 'assets/Free_Test_video.mp4'
+        else:
+            self.video_src = 'assets/big_buck_bunny_720p_1mb.mp4'
+    
+    # handles the event simulation of the sensor trigger event
+    def button_clicked(self):
+        self.ads_component_handler()
+        self.video_component_handler()
+
 
 class DriveThruApp(App):
     def build(self):
