be6e99385950c192509b4775df503f459c695d52
OrangePi Demo <fragnatic62@gmail.com>
2023-04-14 03:58:25 +0800

    add orange pi pin event

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 
+# Kivy imports
 from kivy.core.window import Window
 from kivy.app import App
 from kivy.uix.widget import Widget
@@ -8,6 +9,20 @@ from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.image import Image
 from kivy.uix.video import Video
+from kivy.clock import Clock
+
+# Orange Pi Pins imports
+from pyA20.gpio import gpio
+from pyA20.gpio import port
+from time import sleep
+
+#initialize the gpio module
+gpio.init()
+
+#initialize GPIO pin
+gpio.setcfg(port.PA12,gpio.INPUT)
+gpio.pullup(port.PA12,gpio.PULLUP)
+gpio.pullup(port.PA12,gpio.PULLDOWN)
 
 Window.maximize()
 
@@ -20,25 +35,31 @@ class AppLayout(Widget):
     img_src = StringProperty('assets/test-image.png')
     video_src = StringProperty('assets/big_buck_bunny_720p_1mb.mp4')
 
-    # handles the streaming/image event on advertisement component section
-    def ads_component_handler(self):
-        if(self.img_src=='assets/test.png'):
-            self.img_src = 'assets/test-image.png'
-        else:
-            self.img_src = 'assets/test.png'
-
-    # handles the streaming event on customer and ordertaker component section
-    def video_component_handler(self):
-        if(self.video_src=='assets/big_buck_bunny_720p_1mb.mp4'):
-            self.video_src = 'assets/Free_Test_video.mp4'
-        else:
-            self.video_src = 'assets/big_buck_bunny_720p_1mb.mp4'
+    def __init__(self,**kwargs):
+        super(AppLayout,self).__init__(**kwargs)
+        Clock.schedule_interval(self.event_handler,0.1)
     
-    # handles the event simulation of the sensor trigger event
-    def button_clicked(self):
-        self.ads_component_handler()
-        self.video_component_handler()
+    #event callback to change screen view to camera
+    def event_handler(self,dt):
+        value = gpio.input(port.PA12)
+        if(value==0):
+            self.set_screen_to_video_stream()
+            Clock.schedule_once(self.reset_event_to_default,5)
+    
+    #event callback to reset screen view
+    def reset_event_to_default(self,dt):
+        self.set_screen_to_default()
 
+    
+    def set_screen_to_default(self):
+        self.img_src = 'assets/test-image.png'
+        self.video_src = 'assets/big_buck_bunny_720p_1mb.mp4'
+    
+    def set_screen_to_video_stream(self):
+        self.img_src = 'assets/test.png'
+        self.video_src = 'assets/Free_Test_video.mp4'
+
+    
 
 class DriveThruApp(App):
     def build(self):
