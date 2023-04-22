ddd920d8b6777066cac2c2b9e617f998e0831aa0
Douie Villegas <fragnatic62@gmail.com>
2023-04-23 05:46:04 +0800

    update server ip

--- api/stream_request.py
+++ api/stream_request.py
@@ -1,6 +1,6 @@
 import socket
 
-HOST = '127.0.0.1'  # The server's hostname or IP address
+HOST = '192.168.1.107'  # The server's hostname or IP address
 PORT = 8000        # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
